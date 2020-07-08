1d6fb944d22d71e8aba00b158537d778c5b50d55
InterviewTestSolutions <interviewtestsolutions@mail.bg>
2020-07-08 11:33:36 +0200

    Added Json file reader

---
+++ FileReader/FileReader/JsonFileReader.cs
@@ -0,0 +1,30 @@
+﻿using FileReader.Interfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileReader
+{
+    public sealed class JsonFileReader : IFileReader
+    {
+        private readonly string filePath;
+
+        public JsonFileReader(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public string ReadFile()
+        {
+            var text = File.ReadAllText(filePath);
+
+            var json = JObject.Parse(text);
+
+            string result = (string)json["contents"];
+            return result;
+        }
+    }
+}
